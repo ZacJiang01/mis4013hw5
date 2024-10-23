7f35f131ccdc5de05276d8553c09f9f4200cfe1d
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-23 13:35:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
   <body>
     <h1>Homework 5</h1>
     <div><a href="page1.php">Page 1</a></div>
-    
-
+    <div><a href="page2.php">Page 2</a></div>
+    <div><a href="page3.php">Page 3</a></div>
+    <div><a href="page4.php">Page 4</a></div>
+    <div><a href="page5.php">Page 5</a></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
